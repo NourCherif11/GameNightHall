6016c56c6449d412c9b685ee4725cfe8fdf2ace3
Cherif Nour <cherifnour@isimsf.u-sfax.tn>
2026-03-12 19:08:52 +0100

    fix: update base URL to GameNightHall for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Billard-and-Playstation-Management-Application/',
+  base: '/GameNightHall/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
